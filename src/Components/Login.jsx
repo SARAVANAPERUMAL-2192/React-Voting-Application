0cc0bf77b80e7826287bda816b0c134db1709c91
SARAVANAPERUMAL-2192 <109029826+SARAVANAPERUMAL-2192@users.noreply.github.com>
2024-03-06 20:35:36 +0530

    Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,12 +1,24 @@
 import React from "react";
-
+import LogoImg from "../assets/Logo.jpg";
+import Bgimg from "../assets/Bg.jpg"
 const Login = (props) => {
-    return (
-        <div className="login-container">
-            <h1 className="welcome-message">Welcome to voting application</h1>
-            <button className="login-button" onClick = {props.connectWallet}>Login Metamask</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="header"> {/* Add the 'header' class */}
+        <img className="header-img" src={LogoImg} alt="Logo" /> {/* Use 'header-img' class */}
+        <h1 className="font-semibold text-2xl">Election 2024</h1>
+      </div>
+      <div className="bg-container"> {/* Add a container for the background image */}
+        <img className="bg-img" src={Bgimg} alt="Background" /> {/* Use 'bg-img' class */}
+        <div className="text-overlay">
+            <div className="white-bg">
+                <h1 className="welcome-message">Welcome to the voting application</h1>
+                <button className="login-button" onClick={props.connectWallet}>Login Metamask</button>
+            </div>
+        </div> 
+      </div>
+    </div>
+  );
+};
 
 export default Login;
