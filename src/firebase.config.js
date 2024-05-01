9c6da83fc000b097a7d87709f3f0e01c5f077d65
SARAVANAPERUMAL-2192 <crjsp2004@gmail.com>
2024-05-01 16:08:33 +0530

    added analytics

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD_3EFNu7tZt1fFBquX3GUYX-FRoSJNdRo",
-  authDomain: "sfffsf.firebaseapp.com",
-  projectId: "sfffsf",
-  storageBucket: "sfffsf.appspot.com",
-  messagingSenderId: "720703945005",
-  appId: "1:720703945005:web:60247709c3190e32077ba4",
-  measurementId: "G-SEYQ51HJ4G"
+  apiKey: "AIzaSyDCG-JshfMY9MtWAGsosx-MAgFnnkSKV2c",
+  authDomain: "ttthdsrg.firebaseapp.com",
+  projectId: "ttthdsrg",
+  storageBucket: "ttthdsrg.appspot.com",
+  messagingSenderId: "114754618504",
+  appId: "1:114754618504:web:e50af9d4f227ac7cfe0b3d",
+  measurementId: "G-4DWTG3RJJY"
 };
 
 // Initialize Firebase
